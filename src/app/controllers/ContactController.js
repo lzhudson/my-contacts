ca0f9311a23168f94ea907bd58ce143ad357d156
Hudson Holanda <hudsonholanda55@gmail.com>
2021-12-08 18:47:51 -0300

    feat: create method register contact

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -17,8 +17,22 @@ class ContactController {
     return response.json(contact);
   }
 
-  store() {
+  async store(request, response) {
     // post one register
+    const {
+      name, phone, email, category_id,
+    } = request.body;
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+    const contactExists = await ContactsRepository.findByEmail(email);
+    if (contactExists) {
+      return response.status(400).json({ error: 'This is e-mail is already been taken' });
+    }
+    const contact = await ContactsRepository.create({
+      name, phone, email, category_id,
+    });
+    return response.json(contact);
   }
 
   update() {

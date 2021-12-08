3461857529d0671424bd6a13cad197a49044c42c
Hudson Holanda <hudsonholanda55@gmail.com>
2021-12-08 11:00:15 -0300

    feat: create method findById to get contact with id

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -7,10 +7,22 @@ const contacts = [
     email: 'hudson@gmail.com',
     category_id: v4(),
   },
+  {
+    id: v4(),
+    name: 'Ana Lis',
+    email: 'analist@gmail.com',
+    category_id: v4(),
+  },
 ];
 class ContactsRepository {
   findAll() {
     return new Promise((resolve) => resolve(contacts));
+  }
+
+  findById(id) {
+    return new Promise((resolve) => resolve(
+      contacts.find((contact) => contact.id === id),
+    ));
   }
 }
 

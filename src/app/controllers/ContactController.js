38789c267efc1789bdda131a74c8623e0e095082
Hudson Holanda <hudsonholanda55@gmail.com>
2021-12-08 11:07:27 -0300

    feat: implement method delete to delete one register

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -25,8 +25,15 @@ class ContactController {
     // update register
   }
 
-  delete() {
+  async delete(request, response) {
     // delete register
+    const { id } = request.params;
+    const contact = await ContactsRepository.findById(id);
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+    await ContactsRepository.delete(id);
+    return response.sendStatus(204);
   }
 }
 // Sigleton

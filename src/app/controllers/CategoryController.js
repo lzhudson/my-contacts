9c6fd49d2bd7276b085b13143e6271228512dd7f
Hudson Holanda <hudsonholanda55@gmail.com>
2021-12-10 21:48:35 -0300

    feat: create method to show one categoy by id

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -6,6 +6,15 @@ class CategoryController {
     return response.json(categories);
   }
 
+  async show(request, response) {
+    const { id } = request.params;
+    const category = await CategoriesRepository.findById(id);
+    if (!category) {
+      return response.status(404).json({ error: 'Category not found' });
+    }
+    return response.json(category);
+  }
+
   async store(request, response) {
     const { name } = request.body;
 

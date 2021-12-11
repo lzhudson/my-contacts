8edcf3dd6e00cbb89bf2b373831713f495a40a02
Hudson Holanda <hudsonholanda55@gmail.com>
2021-12-10 21:15:14 -0300

    feat(refact): update methode delete to exclude from database

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,7 +1,7 @@
 const { v4 } = require('uuid');
 const db = require('../../database');
 
-let contacts = [
+const contacts = [
   {
     id: v4(),
     name: 'Hudson Holanda',
@@ -32,11 +32,9 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
+  async delete(id) {
+    const deleteOp = await db.query('DELETE FROM contacts WHERE id = $1', [id]);
+    return deleteOp;
   }
 
   async create({

86e80a0b201b90c80d41e6bfad11f08fa16e1e32
Hudson Holanda <hudsonholanda55@gmail.com>
2021-12-10 22:01:50 -0300

    feat: create method update

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -26,6 +26,26 @@ class CategoryController {
 
     return response.json(category);
   }
+
+  async update(request, response) {
+    const { id } = request.params;
+
+    const categoryExists = await CategoriesRepository.findById(id);
+
+    if (!categoryExists) {
+      return response.status(400).json({ error: 'Category not found' });
+    }
+
+    const { name } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const category = await CategoriesRepository.update(id, { name });
+
+    return response.json(category);
+  }
 }
 
 module.exports = new CategoryController();
